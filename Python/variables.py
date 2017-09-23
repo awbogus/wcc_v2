3a199c4b4d0f4a36c3400d8b768d839ade6b4728
Allie Bogus <alexandra.bogus@gmail.com>
2017-09-22 22:49:00 -0400

    variables checkpoint

---
+++ Python/variables.py
@@ -0,0 +1,19 @@
+x = 4
+y = x + 1
+
+print(x)
+print(y)
+
+first_name = 'Allie'
+last_name = 'Bogus'
+
+print('My first name is ' + first_name + '.')
+print('And my last name is ' + last_name + '.')
+print ('Together, I am ' + first_name + ' ' + last_name)
+
+age = 30
+next_age = age + 1
+birth_month = 'August'
+birth_stone = 'peridot'
+
+print('Ariel is ' + str(age) + ' years old, but she will turn ' + str(next_age) + ' in August. Her birth stone is ' + birth_stone + '.' )
